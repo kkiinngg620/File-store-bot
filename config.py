4690d83b994b10000e59a398db96551ad28681c0
kkiinngg620 <133698348+kkiinngg620@users.noreply.github.com>
2023-07-18 20:12:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", None)
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID")
+API_ID = int(os.environ.get("API_ID", "27157998"))
+API_HASH = os.environ.get("API_HASH", "45d09c93e37c9b93b6535949c898f906")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6300428067:AAHs7bNn1eFkcRUaWTo3b23Q3YJXfftkvcE")
+DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "-1001900692143")
 IS_PRIVATE = os.environ.get("IS_PRIVATE",False) # any input is ok But True preferable
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-PROTECT_CONTENT = True
-UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', '')
+OWNER_ID = int(os.environ.get("OWNER_ID", "1452198353"))
+PROTECT_CONTENT = False
+UPDATE_CHANNEL = os.environ.get('UPDATE_CHANNEL', 'moviieeadda3')
 AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS") else []
 if OWNER_ID not in AUTH_USERS:
     AUTH_USERS.append(OWNER_ID)
